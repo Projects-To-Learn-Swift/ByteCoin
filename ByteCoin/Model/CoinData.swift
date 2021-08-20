a937e265fe0bbe36f885b1c0f490f5e1e0501b8e
Nikki Personal <nikkipersonal@NRae-PL.local>
2021-08-20 12:20:35 +0100

    First commit

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Nikki Personal on 7/30/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
